dc6394abbdec5e48da6f3bcf85048708b7adc676
rodgersxy <rodgersny99@gmail.com>
2024-02-14 11:42:07 +0300

    File data

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
 
 const express = require('express');
 
@@ -16,6 +17,12 @@ const router = (app) => {
   paths.get('/connect', ((request, response) => AuthController.getConnect(request, response)));
   paths.get('/disconnect', ((request, response) => AuthController.getDisconnect(request, response)));
   paths.get('/users/me', ((request, response) => UsersController.getMe(request, response)));
+  paths.post('/files', ((request, response) => FilesController.postUpload(request, response)));
+  paths.get('/files/:id', ((request, response) => FilesController.getShow(request, response)));
+  paths.get('/files', ((request, response) => FilesController.getIndex(request, response)));
+  paths.put('/files/:id/publish', ((request, response) => FilesController.putPublish(request, response)));
+  paths.put('/files/:id/unpublish', ((request, response) => FilesController.putUnpublish(request, response)));
+  paths.get('/files/:id/data', ((request, response) => FilesController.getFile(request, response)));
 };
 
 export default router;
